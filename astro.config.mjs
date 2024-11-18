e6d292cd2d87963ec5f2ed77eff77995bef7f35a
meinrehlein <155490503+meinrehlein@users.noreply.github.com>
2024-11-18 11:28:24 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-
-site: 'https://meinrehlein.github.io',
-
-});
+  site: 'https.jannisreinelt.com',
+})
